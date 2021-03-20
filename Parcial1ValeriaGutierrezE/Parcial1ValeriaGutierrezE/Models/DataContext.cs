072bafde14721eecd3848ea6b6b865473e9f3128
ValeriaGutierrez <ValeGe@DESKTOP-VESR4FH>
2021-03-20 11:14:59 -0400

    Se agrego base y DataContext

---
+++ Parcial1ValeriaGutierrezE/Parcial1ValeriaGutierrezE/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1ValeriaGutierrezE.Models
+{
+    public class DataContext : DbContext 
+    {
+    }
+}
