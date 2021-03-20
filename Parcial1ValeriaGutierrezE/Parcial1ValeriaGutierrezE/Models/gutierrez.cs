1361a0ac3910e2a47ce25270adee3c71d014aa0f
ValeriaGutierrez <ValeGe@DESKTOP-VESR4FH>
2021-03-20 10:49:38 -0400

    Creando modelo

---
+++ Parcial1ValeriaGutierrezE/Parcial1ValeriaGutierrezE/Models/gutierrez.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1ValeriaGutierrezE.Models
+{
+    public class gutierrez
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(30,MinimumLength =3,ErrorMessage ="Ingrese longitud entre 3 a 30 caracteres")]
+        public double Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM/dd}")]
+        public DateTime LastBuy { get; set; }
+
+    }
+}
